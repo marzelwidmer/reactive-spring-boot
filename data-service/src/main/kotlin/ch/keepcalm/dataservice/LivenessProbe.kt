eabac95601b0d7442c7a47d06d6ae12300b93d80
Marcel Widmer <marzelwidmer@gmail.com>
2020-03-22 19:26:52 +0100

    Add Data Service

---
+++ data-service/src/main/kotlin/ch/keepcalm/dataservice/LivenessProbe.kt
@@ -0,0 +1,10 @@
+package ch.keepcalm.dataservice
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class LivenessProbe {
+    @GetMapping(value = ["/alive"])
+    fun alive() = "ok"
+}
