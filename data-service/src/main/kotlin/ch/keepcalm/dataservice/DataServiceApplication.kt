9f2853c59b0ac83bdc15c15a43e2d95642457547
Marcel Widmer <marzelwidmer@gmail.com>
2020-03-28 17:53:49 +0100

    Configura both mongo and postgres

--- data-service/src/main/kotlin/ch/keepcalm/dataservice/DataServiceApplication.kt
+++ data-service/src/main/kotlin/ch/keepcalm/dataservice/DataServiceApplication.kt
@@ -1,16 +1,13 @@
 package ch.keepcalm.dataservice
 
-//import org.springframework.data.r2dbc.repository.Query
-import io.r2dbc.spi.ConnectionFactory
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.context.event.EventListener
-import org.springframework.context.support.beans
 import org.springframework.data.annotation.Id
-import org.springframework.data.r2dbc.connectionfactory.R2dbcTransactionManager
+import org.springframework.data.annotation.TypeAlias
+import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.r2dbc.core.DatabaseClient
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.relational.core.mapping.Column
@@ -20,32 +17,64 @@ import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import org.springframework.transaction.reactive.TransactionalOperator
 import reactor.core.publisher.Flux
+import reactor.kotlin.core.publisher.toFlux
+import java.util.*
 
 @SpringBootApplication
-class DataServiceApplication {
-
-    @Bean
-    fun r2dbcTransactionManager(cf: ConnectionFactory): R2dbcTransactionManager {
-        return R2dbcTransactionManager(cf)
-    }
-}
+class DataServiceApplication
 
 fun main(args: Array<String>) {
-    runApplication<DataServiceApplication>(*args) {
-        val context = beans {
-            bean {
-                // R2dbcTransactionManager()
-            }
-        }
-        addInitializers(context)
+    runApplication<DataServiceApplication>(*args)
+}
+
+@Component
+class SampleDataInitializer(private val reservationMongoRepository: ReservationMongoRepository, private val reservationRepository: ReservationRepository, private val databaseClient: DatabaseClient,
+        private val reservationService: ReservationService) {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+
+    @EventListener(classes = [ApplicationReadyEvent::class])
+    fun ready() {
+
+        // LowLevel Database Clientx,
+        // Read Database Entries
+        this.databaseClient
+            .select().from("reservation").`as`(Reservation::class.java)
+            .fetch()
+            .all()
+            .doOnComplete { logger.info("------------------------------------") }
+            .subscribe { logger.info("<-- $it") }
+
+        // Save List Transactional
+        val reservations = reservationService.saveAll("Madhura", "Josh", "Olga", "Marcin", "Stéphane", "Violetta", "Ria", "Dr. Syer")
+        // Transaction Service is used...
+        //        val reservations = Flux.just("Madhura", "Josh", "Olga", "Marcin", "Stéphane", "Violetta", "Ria", "Dr. Syer")
+        //            .map { name -> Reservation(id = null, name = name) }
+        //                .flatMapSequential { reservationRepository.save(it) }
+        //            .flatMap { reservationRepository.save(it) }
+        //
+        reservationRepository
+            .deleteAll()
+            .thenMany(reservations)
+            .thenMany(reservationRepository.findAll())
+            .subscribe { reservation -> logger.info("---> $reservation") }
+
+
+        // MongoDB
+        reservationMongoRepository.deleteAll().thenMany(
+            listOf("John", "Jane", "Roger", "James", "Bob", "Jack")
+                .toFlux()
+                .map { ReservationMongo(name = it) })
+            .flatMap { reservationMongoRepository.save(it) }
+            .thenMany(reservationMongoRepository.findAll())
+            .subscribe { println(it) }
     }
+
 }
 
 @Service
-class ReservationService(
-    private val reservationRepository: ReservationRepository,
-    private val transactionalOperator: TransactionalOperator
-) {
+class ReservationService(private val reservationRepository: ReservationRepository, private val transactionalOperator: TransactionalOperator) {
 
     fun saveAll(vararg names: String?): Flux<Reservation> {
         val reservations = Flux.fromArray(names)
@@ -55,55 +84,14 @@ class ReservationService(
     }
 }
 
-@Component
-class SampleDataInitializer(private val reservationRepository: ReservationRepository, private val databaseClient: DatabaseClient,
-                            private val reservationService: ReservationService) {
-
-    private val logger = LoggerFactory.getLogger(javaClass)
-
-
-    @EventListener(classes = [ApplicationReadyEvent::class])
-    fun ready() {
-
-        // LowLevel Database Client
-        // Read Database Entries
-        this.databaseClient
-            .select().from("reservation").`as`(Reservation::class.java)
-            .fetch()
-            .all()
-            .doOnComplete { logger.info("------------------------------------") }
-            .subscribe { logger.info("<-- $it") }
-
-        val reservations = reservationService.saveAll("Foo", "Bar")
-        // Transaction Service is used...
-//        val reservations = Flux.just("Madhura", "Josh", "Olga", "Marcin", "Stéphane", "Violetta", "Ria", "Dr. Syer")
-//            .map { name -> Reservation(id = null, name = name) }
-//                .flatMapSequential { reservationRepository.save(it) }
-//            .flatMap { reservationRepository.save(it) }
-//
-        reservationRepository
-            .deleteAll()
-            .thenMany(reservations)
-            .thenMany(reservationRepository.findAll())
-            .subscribe { reservation -> logger.info("---> $reservation") }
-
-        //  Shortform
-//        reservationRepository.deleteAll()
-//                .thenMany(listOf("Madhura", "Josh", "Olga", "Marcin", "Stéphane", "Violetta", "Ria", "Dr. Syer").toFlux().map { Reseration(name = it) })
-//                .flatMap { reservationRepository.save(it) }
-//                .thenMany(reservationRepository.findAll())
-//                .subscribe { println(it) }
-    }
-
+// MongoDB
+interface ReservationMongoRepository : ReactiveCrudRepository<ReservationMongo, String> {
+    fun findByName(name: String): Flux<Reservation>
 }
 
-// MongoDB
-//interface ReservationRepository : ReactiveCrudRepository<Reservation, String> {
-//    fun findByName(name: String): Flux<Reservation>
-//}
-//@Document
-//@TypeAlias("reservation")
-//data class Reservation(@Id val id: String = UUID.randomUUID().toString(), val name: String)
+@Document
+@TypeAlias("reservation")
+data class ReservationMongo(@Id val id: String = UUID.randomUUID().toString(), val name: String)
 
 
 // Postgres
