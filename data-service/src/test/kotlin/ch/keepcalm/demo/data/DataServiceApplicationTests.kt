221fe17d58d7f74655b4182f4aba5746a293cb0d
Marcel Widmer <marzelwidmer@gmail.com>
2020-04-09 09:44:48 +0200

    Add TestInstance Lifecycle Per Class Annotation

--- data-service/src/test/kotlin/ch/keepcalm/demo/data/DataServiceApplicationTests.kt
+++ data-service/src/test/kotlin/ch/keepcalm/demo/data/DataServiceApplicationTests.kt
@@ -1,9 +1,11 @@
 package ch.keepcalm.demo.data
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DataServiceApplicationTests {
 
 	@Test
--- websocket-service/src/test/kotlin/ch/keepcalm/demo/websocket/WebSocketApplicationTests.kt
+++ websocket-service/src/test/kotlin/ch/keepcalm/demo/websocket/WebSocketApplicationTests.kt
@@ -1,9 +1,11 @@
 package ch.keepcalm.demo.websocket
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class WebSocketApplicationTests {
 
 	@Test
