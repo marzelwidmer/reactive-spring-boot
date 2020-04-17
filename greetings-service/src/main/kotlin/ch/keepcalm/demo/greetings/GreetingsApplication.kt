4177d2a591e55d9e0b77e529c3a7a66ef7366fad
Marcel Widmer <marzelwidmer@gmail.com>
2020-04-17 22:58:55 +0200

    WIP : CircuitBreaker issues...

--- greetings-service/src/main/kotlin/ch/keepcalm/demo/greetings/GreetingsApplication.kt
+++ greetings-service/src/main/kotlin/ch/keepcalm/demo/greetings/GreetingsApplication.kt
@@ -4,37 +4,48 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory
 import org.springframework.context.event.EventListener
+import org.springframework.context.support.beans
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 import reactor.util.retry.Retry
 import java.time.Duration
 
-
 @SpringBootApplication
-class GreetingsApplication {
-    @Bean
-    fun webClient(builder: WebClient.Builder): WebClient {
-        return builder.baseUrl("http://localhost:8080")
-            //.filter(ExchangeFilterFunctions.basicAuthentication())Å
-            .build()
+class GreetingsApplication
+
+fun main(args: Array<String>) {
+    runApplication<GreetingsApplication>(*args) {
+
+        addInitializers(
+            beans {
+                bean {
+                    WebClient.builder()
+                        .baseUrl("http://localhost:8080")
+                        .build()
+                }
+                bean {
+                    ReactiveResilience4JCircuitBreakerFactory()
+                }
+            }
+        )
     }
 }
 
-fun main(args: Array<String>) {
-    runApplication<GreetingsApplication>(*args)
-}
-
 @Component
-class Client(private val webClient: WebClient) {
+class Client(private val webClient: WebClient
+    , private val reactiveCircuitBreakerFactory: ReactiveResilience4JCircuitBreakerFactory
+) {
 
     private val log = LoggerFactory.getLogger(javaClass)
+
 
     @EventListener(classes = [ApplicationReadyEvent::class])
     fun ready(): Unit {
         val name = "SpringFans"
+
         webClient
             .get()
             .uri("/greeting/{name}", name)
@@ -51,15 +62,30 @@ class Client(private val webClient: WebClient) {
             }
             .subscribe { log.info("--> Mono: $it") }
 
-        webClient
+
+        // CircuitBreaker
+
+        val http = webClient
             .get()
-            .uri("/greetings/{name}", name)
+            .uri("/greeting/{name}", name)
             .retrieve()
-            .bodyToFlux(GreetingResponse::class.java)
-            .subscribe { log.info("--> Flux: ${it.message}") }
+            .bodyToMono(GreetingResponse::class.java)
+            .map(GreetingResponse::message)
+
+        reactiveCircuitBreakerFactory
+            .create("greeting")
+            .run(http) {
+                Mono.just("Ooopss CircuitBreaker !!! ${it.message} ")
+            }
+            .subscribe { log.info("--> CircuitBreaker Mono: $it") }
+
+
     }
+
+
 }
 
 
 data class GreetingRequest(val name: String)
 data class GreetingResponse(val message: String)
+
